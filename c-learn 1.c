d8c6cb80f2cb5281df39c2567f60615efb1f1e62
Timothy3600 <timothybwire55@gmail.com>
2024-11-18 23:37:20 -0800

    Add files via upload

---
+++ c-learn 1.c
@@ -0,0 +1,40 @@
+#include<stdio.h>
+
+int main() {
+   /* int score =52;
+
+    if (score >=80){
+    printf("High distinction");
+    }
+    else if(score >=70){
+    printf("Distinction");
+    }
+    if(score >=60){
+            printf("credit");
+    }
+    else if(score>=50){
+            printf("pass");
+    }
+    else {
+            printf("fail");
+    }*/
+    int day=3;
+    switch(day){
+case 1:
+    printf("Today is Monday");
+    break;
+case 2:
+    printf("Today is Tuesday");
+    break;
+case 3:
+    printf("Today is Wdnesday");
+    break;
+default:
+        printf("No such day in a week");
+        }
+
+
+
+
+return 0;
+}
