e82427b0c8ab56fbec5855077272034c38fa51fd
Timothy3600 <timothybwire55@gmail.com>
2024-11-19 23:15:54 -0800

    Add files via upload

---
+++ c-learn 2.0..c
@@ -0,0 +1,19 @@
+    #include <stdio.h>
+
+    int main() {
+    /*int age;
+    printf("Please enter your age below\n");
+    scanf("%d",&age);
+    if(age>60){
+    printf("you are an old person");
+    }
+    else if(age>=18 &&age<=60){
+    printf("You are a youth");
+    }
+     else{
+      printf("You are a child");
+     }*/
+
+
+return 0;
+}
