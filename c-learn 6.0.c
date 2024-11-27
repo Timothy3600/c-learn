981658caa962233064004773f12bc8ed3bda1a7d
Timothy3600 <timothybwire55@gmail.com>
2024-11-26 23:46:00 -0800

    Added c-learn
    Added file c-learn to my repo containing do/while loops and complied it with a .exe

---
+++ c-learn 6.0.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+int main() {
+    /*int x,a,b,sum;
+    do{
+        printf("Please enter the first number\n");
+        scanf("%d",&a);
+        printf("Please enter a second number|n");
+        scanf("%d",&b);
+        sum=a+b;
+        printf("The sum of %d and %d is %d\n",a,b,sum);
+        printf("would you like to continue??\n press to continue or  to exit");
+        scanf("%d",&x);
+          }
+    while(x==1);
+*/
+int i, x, y, sum;
+ i=1;
+ do{
+        sum=x+y;
+ printf("The sum is %d\n",sum);
+ i++;
+ }
+ while(i<=5);
+
+return 0;
+}
