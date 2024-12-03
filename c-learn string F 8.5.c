26cfc4bc7901679b9432c5c2dd81e21ae2f5b822
Timothy3600 <timothybwire55@gmail.com>
2024-12-02 23:58:57 -0800

    String Functions.
    Added files containing string functions.

---
+++ c-learn string F 8.5.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+void name (int age, int student_number){
+    printf("I'm %d years old and my student number is %d",age,student_number);
+}
+//This is the main function
+int main(){
+    printf("I'm 10 years old\n");
+    name(10,123435679);
+    number(012240197);
+return 0;
+}
